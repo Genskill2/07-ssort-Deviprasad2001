df36a3fe4f6124215bc6b9833dec0c5851e3d27b
Deviprasad2001 <85060262+Deviprasad2001@users.noreply.github.com>
2021-07-01 20:40:21 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,4 +1,4 @@
-void swap_max(int A[], int n, int m)
+void swap_max(int A[], int m, int n)
 {  int largenum,pos,temp;
     largenum=A[m];
    for(int i=m; i<n;i++)
@@ -13,7 +13,12 @@ void swap_max(int A[], int n, int m)
     A[m]=A[pos];
     A[pos]=temp;
 }
-
+void ssort(int A[], int n)
+{ 
+  for(int i=0;i<n;i++)
+  { swap_max(A,1,i);
+  }
+}    
     
     
       
