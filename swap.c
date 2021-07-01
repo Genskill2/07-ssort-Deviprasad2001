57019cb7dd5aa0a88a8582f26f480b7120cd36c3
Deviprasad2001 <85060262+Deviprasad2001@users.noreply.github.com>
2021-07-01 21:29:07 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,24 +1,27 @@
-void swap_max(int A[], int m, int n)
+  void swap_max(int A[], int n, int m)
 {  int largenum,pos,temp;
+   int wide = n-m;
     largenum=A[m];
-   for(int i=m; i<n;i++)
+   for(int i=0; i<wide;i++)
     {  
-        if(A[i+1]>largenum)
-          { largenum = A[i+1];
-             pos = i+1;
+        if(A[(m+i)]>largenum)
+          { largenum = A[(m+i)];
+             pos = m+i;
           }
    
      }  
+  if(largenum>A[m])  
+  {
     temp=A[m];
     A[m]=A[pos];
     A[pos]=temp;
+  }
 }
 void ssort(int A[], int n)
-{ 
+{  
   for(int i=0;i<n;i++)
-  { swap_max(A,1,i);
+  { swap_max(A,n-1,i-1);
   }
 }    
-    
     
       
